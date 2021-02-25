d2926bc45a18b2daf8995185b598788a868a3aef
kmayika <kwezimay@gmail.com>
2021-02-25 10:05:05 +0200

    first commit

---
+++ tests/example.test.js
@@ -0,0 +1,8 @@
+module.exports = {
+    "This is a nightwatch test": (client) => {
+        client
+          .url('http://www.takealot.com')
+          .waitForElementVisible('body', 1000)
+          .assert.visible('body', '[YES]');
+    }
+};
