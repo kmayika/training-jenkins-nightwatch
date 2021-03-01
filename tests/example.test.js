540f0246fa6d634ff3ca7612e4acdf2c9f0ddc0a
kmayika <kwezimay@gmail.com>
2021-03-01 13:46:53 +0200

    Jenkinsfile 9

--- tests/example.test.js
+++ tests/example.test.js
@@ -1,7 +1,7 @@
 module.exports = {
     "This is a nightwatch test": (client) => {
         client
-          .url('http://www.takealot.com')
+          .url(`http://www.${process.env.ENVIRONMENT_URL}`)
           .waitForElementVisible('body', 1000)
           .assert.visible('body', `hub host : ${process.env.HUB_HOST}`)
           .pause(5000);
